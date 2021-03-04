cfce56a64275d8c235c2ffcf98054b32af748d4f
luukas <lukasdoesdev@gmail.com>
2021-03-04 17:51:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# archpresence
+
+## Usage:
+```sh
+# Install virtualenv globally
+pip install virtualenv
+
+# Create a new virtual enviroment
+virtualenv venv
+
+# Activate the virtual enviroment
+# You should see "(venv)" appear before your prompt
+source venv/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
+```
+
+### How to exit venv:
+Type `deactivate` in the terminal.
+
+### How to get back to the venv to update requirements, run script, etc:
+Type `source venv/bin/activate` in the terminal.
---
+++ main.py
@@ -0,0 +1,35 @@
+#!/bin/env python
+
+from pypresence import Presence
+import time
+import psutil
+import subprocess
+
+client_id = "817047477718351893" # Lukas client id, this hosts the arch logo
+RPC = Presence(client_id)  # Initialize the Presence client
+
+kernel_cmd = subprocess.run(["uname", "-r"], capture_output=True) # Get kernel version
+kernel = str(kernel_cmd.stdout, 'UTF-8') # Convert to string
+
+RPC.connect() # Start the handshake loop
+RPC.update(
+    details="Linux " + kernel,
+    state="Windows? Ew.",
+    large_image="archsvg",
+    large_text="Arch Linux Logo",
+    start=psutil.boot_time(),
+    buttons=[
+        {
+            'label': 'Install Arch Linux',
+            'url': 'https://archlinux.org'
+        },
+        {
+            'label': 'What\'s Linux?',
+            'url': 'https://en.wikipedia.org/wiki/Linux'
+        }
+    ]
+) # Updates our presence
+
+while True:
+    time.sleep(15)
+
